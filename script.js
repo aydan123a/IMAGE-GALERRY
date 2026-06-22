864f760e53ffab26d52807a6872a5843123ce60a
aydan123a <aydanhaciyeva589@gmail.com>
2026-06-22 11:11:48 +0400

    Add lightbox functionality for image display

---
+++ script.js
@@ -0,0 +1,28 @@
+const images=[
+   "images/download (1).webp" ,
+    "images/download (2).webp" ,
+    "images/OIP (1).webp" ,
+    "images/OIP (2).webp",
+    "images/OIP.webp" ,
+];
+let currentIndex=0;
+function openLightbox(index){
+    currentIndex=index;
+    document.getElementById("lightbox").style.display="block";
+    document.getElementById("lightbox-img").src=images[currentIndex];
+
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+function changeImage(step){
+    currentIndex+=step;
+    if(currentIndex<0){
+        currentIndex=images.length-1;
+    }
+    if(currentIndex>=images.length){
+        currentIndex=0;
+    }
+    document.getElementById("lightbox-img").src=images[currentIndex];
+}
